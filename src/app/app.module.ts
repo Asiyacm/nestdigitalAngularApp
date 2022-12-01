378834a3d91aab63d271d681daecc629d39ff802
Asiyacm <asiyacmuhammed@gmail.com>
2022-12-01 11:00:07 +0530

    gallery commit

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -5,12 +5,14 @@ import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
 import { HomeComponent } from './home/home.component';
 import { AboutUsComponent } from './about-us/about-us.component';
+import { GalleryComponent } from './gallery/gallery.component';
 
 @NgModule({
   declarations: [
     AppComponent,
     HomeComponent,
-    AboutUsComponent
+    AboutUsComponent,
+    GalleryComponent
   ],
   imports: [
     BrowserModule,
