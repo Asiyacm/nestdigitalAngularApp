8da9acbbd5b51d5b715993c7d648d9cd3300ba8b
Asiyacm <asiyacmuhammed@gmail.com>
2022-12-01 20:20:48 +0530

    navigation3 commit

---
+++ src/app/navigation3/navigation3.component.ts
@@ -0,0 +1,10 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'app-navigation3',
+  templateUrl: './navigation3.component.html',
+  styleUrls: ['./navigation3.component.css']
+})
+export class Navigation3Component {
+
+}
