b5a2cb97d721493863fa971a77c4d9a21397145b
Asiyacm <asiyacmuhammed@gmail.com>
2022-12-01 11:32:34 +0530

    adminLogin

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -7,6 +7,7 @@ import { HomeComponent } from './home/home.component';
 import { AboutUsComponent } from './about-us/about-us.component';
 import { GalleryComponent } from './gallery/gallery.component';
 import { ContactUsComponent } from './contact-us/contact-us.component';
+import { AdminLoginComponent } from './admin-login/admin-login.component';
 
 @NgModule({
   declarations: [
@@ -14,7 +15,8 @@ import { ContactUsComponent } from './contact-us/contact-us.component';
     HomeComponent,
     AboutUsComponent,
     GalleryComponent,
-    ContactUsComponent
+    ContactUsComponent,
+    AdminLoginComponent
   ],
   imports: [
     BrowserModule,
